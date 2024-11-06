378649acea219732ac9262589615e3253ddc9bd9
Bagas Aryo <bagas.priyatama04@gmail.com>
2024-11-06 11:39:39 +0700

    terdapat warning sebelumnya, menghapus unused import 'package:perangkat_berangkat/main.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,8 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:perangkat_bergerak/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
